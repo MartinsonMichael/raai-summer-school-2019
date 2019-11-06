b8747301607904acc6b163ab64bb6c3fbce9759b
Martinson Michael <mmartinson@yandex.ru>
2019-11-06 17:11:50 +0300

    change gitignore

--- simulator/env_wrappers.py
+++ simulator/env_wrappers.py
@@ -1,50 +1,20 @@
-import time
-import copy
-import math
-import datetime
-from collections import OrderedDict
-from logging import getLogger
+import functools
+import random
+import string
+from time import sleep
 
+import chainerrl
 import cv2
 import gym
-from gym import spaces
 import numpy as np
-from gym.wrappers import Monitor
-from gym.wrappers.monitoring.stats_recorder import StatsRecorder
-
-import pickle
-import cloudpickle
-from multiprocessing import Process, Pipe, Pool
-
-import gym_car_intersect
-from tqdm import trange
 import collections
 
-
-cv2.ocl.setUseOpenCL(False)
-logger = getLogger(__name__)
-
-# Just copied environments:
-
-class FireResetEnv(gym.Wrapper):
-    def __init__(self, env=None):
-        """For environments where the user need to press FIRE for the game to start."""
-        super(FireResetEnv, self).__init__(env)
-        assert env.unwrapped.get_action_meanings()[1] == 'FIRE'
-        assert len(env.unwrapped.get_action_meanings()) >= 3
-
-    def step(self, action):
-        return self.env.step(action)
-
-    def reset(self):
-        self.env.reset()
-        obs, _, done, _ = self.env.step(1)
-        if done:
-            self.env.reset()
-        obs, _, done, _ = self.env.step(2)
-        if done:
-            self.env.reset()
-        return obs
+from chainerrl.wrappers import atari_wrappers
+from chainerrl.wrappers.atari_wrappers import LazyFrames
+from gym import ActionWrapper
+# noinspection PyUnresolvedReferences
+from gym_car_intersect.envs import CarRacingHackatonContinuous
+from gym import spaces
 
 
 class MaxAndSkipEnv(gym.Wrapper):
@@ -58,6 +28,7 @@ class MaxAndSkipEnv(gym.Wrapper):
     def step(self, action):
         total_reward = 0.0
         done = None
+        info = None
         for _ in range(self._skip):
             obs, reward, done, info = self.env.step(action)
             self._obs_buffer.append(obs)
@@ -75,72 +46,203 @@ class MaxAndSkipEnv(gym.Wrapper):
         return obs
 
 
-class ProcessFrame84(gym.ObservationWrapper):
-    def __init__(self, env=None):
-        super(ProcessFrame84, self).__init__(env)
-        self.observation_space = gym.spaces.Box(low=0, high=255, shape=(300, 300, 1), dtype=np.uint8)
+class DiscreteWrapper(ActionWrapper):
+    def reverse_action(self, action):
+        raise NotImplemented
 
-    def observation(self, obs):
-        return ProcessFrame84.process(obs)
-
-    @staticmethod
-    def process(frame):
-        # if frame.size == 210 * 160 * 3:
-        #     img = np.reshape(frame, [210, 160, 3]).astype(np.float32)
-        # elif frame.size == 250 * 160 * 3:
-        #     img = np.reshape(frame, [250, 160, 3]).astype(np.float32)
-        # elif frame.size == 300 * 300 * 3:
-        #     img = np.reshape(frame, [300, 300, 3]).astype(np.float32)
-        # else:
-        #     assert False, "Unknown resolution."
-        img = np.reshape(frame, [1378, 1378, 3]).astype(np.float32)
-        img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
-        resized_screen = cv2.resize(img, (300, 300), interpolation=cv2.INTER_AREA)
-        x_t = resized_screen#[18:102, :]
-        x_t = np.reshape(x_t, [300, 300])
-        return x_t.astype(np.uint8)
-
-
-class ImageToPyTorch(gym.ObservationWrapper):
     def __init__(self, env):
-        super(ImageToPyTorch, self).__init__(env)
-        old_shape = self.observation_space.shape
-        self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=(old_shape[-1], old_shape[0], old_shape[1]),
-                                                dtype=np.float32)
+        super().__init__(env)
+        self.action_space.n = 3
+        self.action_space = gym.spaces.discrete.Discrete(self.action_space.n)
 
+    def action(self, action):
+        steer = 0.6
+        speed = 0.3
+        if action == 0:
+            return [-steer, speed, 0]
+        if action == 1:
+            return [+steer, speed, 0]
+        if action == 2:
+            return [0.0, speed, 0]
+        if action == 3:
+            # return [0.0, 0, 0]
+            return [0.0, 0.0, 1.0]
+        raise KeyError
+
+
+class CompressWrapper(gym.ObservationWrapper):
     def observation(self, observation):
-        return np.moveaxis(observation, 2, 0)
+        return cv2.resize(observation, dsize=(84, 84), interpolation=cv2.INTER_CUBIC)
 
 
-class ScaledFloatFrame(gym.ObservationWrapper):
-    def observation(self, obs):
-        return np.array(obs).astype(np.float32) / 255.0
+class SkRewardWrapper(gym.RewardWrapper):
+
+    def reward(self, reward):
+        if reward < 0.0:
+            return -1.0
+        else:
+            return reward
 
 
-class BufferWrapper(gym.ObservationWrapper):
-    def __init__(self, env, n_steps, dtype=np.float32):
-        super(BufferWrapper, self).__init__(env)
-        self.dtype = dtype
-        old_space = env.observation_space
-        self.observation_space = gym.spaces.Box(old_space.low.repeat(n_steps, axis=0),
-                                                old_space.high.repeat(n_steps, axis=0), dtype=dtype)
+def random_string(len_=10):
+    letters = 'rewiutopafsdghjklzcxvbnm123565479'
+    return ''.join(random.choice(letters) for _ in range(len_))
+
+
+class SaveWrapper(gym.ActionWrapper, ):
+
+    def __init__(self, env, random_suffix=None):
+        super().__init__(env)
+        self.recording = []
+        self.number = 0
+        self.total_reward = float("-inf")
+        self.best_total_reward = float("-inf")
+        if random_suffix is None:
+            self.random_suffix = random_string(len_=5)
+        else:
+            self.random_suffix = random_suffix
+
+    def step(self, action):
+        obs, reward, done, _ = self.env.step(action)
+        self.total_reward += reward
+        self.recording.append(obs)
+        # draw borders
+        # self.env.training_status()
+        return obs, reward, done, _
 
     def reset(self):
-        self.buffer = np.zeros_like(self.observation_space.low, dtype=self.dtype)
-        return self.observation(self.env.reset())
 
-    def observation(self, observation):
-        self.buffer[:-1] = self.buffer[1:]
-        self.buffer[-1] = observation
-        return self.buffer
+        obs = self.env.reset()
+
+        self.number += 1  #
+        if self.total_reward > self.best_total_reward and self.total_reward > 170:
+            self.best_total_reward = self.total_reward
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            out = cv2.VideoWriter("train/" + str(self.number).zfill(7) + "_" + str(
+                self.total_reward) + "_" + 'recording' + self.random_suffix + '.mp4',
+                                  fourcc, 20.0,
+                                  (obs.shape[1], obs.shape[0]))
+            for image in self.recording:
+                out.write(image)
+            out.release()
+            cv2.destroyAllWindows()
+        self.recording = []
+        self.total_reward = 0
+        return obs
 
 
-def make_pixel_env(name='CartPole-v0'):
-    env = gym.make(name)
-    env = MaxAndSkipEnv(env)
-    # env = FireResetEnv(env)
-    env = ProcessFrame84(env)
-    env = ImageToPyTorch(env)
-    env = BufferWrapper(env, 4)
-    env = ScaledFloatFrame(env)
+class NoopResetEnv(gym.Wrapper):
+    def __init__(self, env, noop_max=30):
+        """Sample initial states by taking random number of no-ops on reset.
+
+        No-op is assumed to be action 0.
+        """
+        gym.Wrapper.__init__(self, env)
+        self.noop_max = noop_max
+        self.override_num_noops = None
+        self.noop_action = 0
+        # assert env.unwrapped.get_action_meanings()[0] == 'NOOP'
+
+    def reset(self, **kwargs):
+        """Do no-op action for a number of steps in [1, noop_max]."""
+        self.env.reset(**kwargs)
+        if self.override_num_noops is not None:
+            noops = self.override_num_noops
+        else:
+            noops = self.unwrapped.np_random.randint(
+                1, self.noop_max + 1)  # pylint: disable=E1101
+        assert noops > 0
+        obs = None
+        for _ in range(noops):
+            obs, _, done, info = self.env.step(self.noop_action)
+            if done or info.get('needs_reset', False):
+                obs = self.env.reset(**kwargs)
+        return obs
+
+    def step(self, ac):
+        return self.env.step(ac)
+
+
+class FrameStackWrapper(gym.Wrapper):
+    def __init__(self, env, stack_size):
+        super().__init__(env)
+        self.stack_size = stack_size
+        self.queue = collections.deque(maxlen=self.stack_size)
+
+    def reset(self, **kwargs):
+        obs = self.env.reset(**kwargs)
+        for _ in range(self.stack_size):
+            self.queue.append(obs)
+        obs = cv2.hconcat(list(self.queue))
+        return obs
+
+    def step(self, action):
+        obs, reward, done, _ = self.env.step(action)
+        self.queue.append(obs)
+        obs = cv2.hconcat(list(self.queue))
+        return obs, reward, done, _
+
+
+def make_car_env(max_frames=15 * 30):
+    print('CarIntersect-v3')
+    env = gym.make('CarIntersect-v3')
+    # env = CompressWrapper(env)
+    env = chainerrl.wrappers.ContinuingTimeLimit(env, max_episode_steps=max_frames)
+    env = MaxAndSkipEnv(env, skip=4)
+    # env = FrameStackWrapper(env, stack_size=4)
+    env = SaveWrapper(env)
+    env = atari_wrappers.wrap_deepmind(env,
+                                       episode_life=False,
+                                       clip_rewards=False, frame_stack=True)
     return env
+
+
+class WarpFrame(gym.ObservationWrapper):
+    def __init__(self, env, channel_order='chw'):
+        """Warp frames to 84x84 as done in the Nature paper and later work.
+
+        To use this wrapper, OpenCV-Python is required.
+        """
+        gym.ObservationWrapper.__init__(self, env)
+        self.width = 84
+        self.height = 84
+        shape = {
+            'hwc': (self.height, self.width, 3),
+            'chw': (3, self.height, self.width),
+        }
+
+        self.observation_space = spaces.Box(
+            low=0, high=255,
+            shape=shape[channel_order], dtype=np.uint8)
+
+        # print("inner", self.observation_space.shape)
+
+    def observation(self, frame):
+        # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        frame = cv2.resize(frame, (self.width, self.height),
+                           interpolation=cv2.INTER_AREA)
+        # print("frame_shape", frame.shape)
+        # print("obs_shape", self.observation_space.low.shape)
+        return frame.reshape(self.observation_space.low.shape)
+
+
+def make_car_env_discrete(max_frames=30 * 30, env_seed=42, random_suffix=None):
+    print('CarIntersect-v3')
+    env = gym.make('CarIntersect-v3')
+    env = chainerrl.wrappers.ContinuingTimeLimit(env, max_episode_steps=max_frames)
+    env = MaxAndSkipEnv(env, skip=4)
+    env = DiscreteWrapper(env)
+    # env = SaveWrapper(env, random_suffix=random_suffix)
+    env = WarpFrame(env)
+    env.seed(env_seed)
+    return env
+
+
+def main():
+    env = make_car_env_discrete()
+    state = env.reset()
+    print(state.shape)
+
+
+if __name__ == '__main__':
+    main()
