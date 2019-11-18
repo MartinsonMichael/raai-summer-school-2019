7daadc6b1894f421ea6fb0721a660aa70631db5d
Martinson Michael <mmartinson@yandex.ru>
2019-11-18 12:13:13 +0300

    cover registration with try section

--- simulator/gym_car_intersect/__init__.py
+++ simulator/gym_car_intersect/__init__.py
@@ -1,22 +1,27 @@
 from gym.envs.registration import register
 
-register(
-    id='CarIntersect-v0',
-    entry_point='gym_car_intersect.envs:CarRacing', #this is function which you want to code to behave
-)
-register(
-    id='CarIntersect-v1',
-    entry_point='gym_car_intersect.envs:CarRacingDiscrete', #this is function which you want to code to behave
-)
-register(
-    id='CarIntersect-v2',
-    entry_point='gym_car_intersect.envs:CarRacingHackaton', #this is function which you want to code to behave
-)
-register(
-    id='CarIntersect-v3',
-    entry_point='gym_car_intersect.envs:CarRacingHackatonContinuous2', #this is function which you want to code to behave
-    max_episode_steps=20 * 1000,
-)
+try:
+    register(
+        id='CarIntersect-v0',
+        entry_point='gym_car_intersect.envs:CarRacing', #this is function which you want to code to behave
+    )
+try:
+    register(
+        id='CarIntersect-v1',
+        entry_point='gym_car_intersect.envs:CarRacingDiscrete', #this is function which you want to code to behave
+    )
+try:
+    register(
+        id='CarIntersect-v2',
+        entry_point='gym_car_intersect.envs:CarRacingHackaton', #this is function which you want to code to behave
+    )
+try:
+    register(
+        id='CarIntersect-v3',
+        entry_point='gym_car_intersect.envs:CarRacingHackatonContinuous2', #this is function which you want to code to behave
+        max_episode_steps=20 * 1000,
+    )
+
 
 # register(
 #     id='CarIntersect-v3.1',
