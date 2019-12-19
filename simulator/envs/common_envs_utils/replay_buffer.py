8d2fa1bdd4437d55b38613bd432270dc29a651ef
Martinson Michael <mmartinson@yandex.ru>
2019-12-19 19:52:44 +0300

    fix

--- simulator/envs/common_envs_utils/replay_buffer.py
+++ simulator/envs/common_envs_utils/replay_buffer.py
@@ -20,8 +20,7 @@ class Replay_Buffer(object):
 
     def add_experience(self, states, actions, rewards, next_states, dones):
         """Adds experience(s) into the replay buffer"""
-        if type(dones) == list:
-            assert type(dones[0]) != list, "A done shouldn't be a list"
+        if isinstance(dones, (list, np.array, np.ndarray)):
             print(states)
             print(dones)
             experiences = [self.experience(state, action, reward, next_state, done)
