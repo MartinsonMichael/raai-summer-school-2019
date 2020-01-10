a89dc997f8032c74a9e9b89e8736d8b0a30c4d33
Martinson Michael <mmartinson@yandex.ru>
2020-01-10 12:31:04 +0300

    fix

--- simulator/envs/common_envs_utils/__init__.py
+++ simulator/envs/common_envs_utils/__init__.py
@@ -1,3 +1,4 @@
 from envs.common_envs_utils.env_wrappers import *
 from envs.common_envs_utils.multiprocessor_env import SubprocVecEnv_tf2
 from envs.common_envs_utils.replay_buffer import Replay_Buffer
+from envs.common_envs_utils.visualizer import visualize
